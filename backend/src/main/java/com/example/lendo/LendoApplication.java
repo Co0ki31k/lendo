390a4e2e289744f771b962edbe632dadcf772e1e
Co0ki3 <mateusz.sempkowski@gmail.com>
2026-04-23 16:42:41 +0200

    feat: update database configuration to use environment variables and load them with dotenv

--- backend/src/main/java/com/example/lendo/LendoApplication.java
+++ backend/src/main/java/com/example/lendo/LendoApplication.java
@@ -1,12 +1,16 @@
 package com.example.lendo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LendoApplication {
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
 		SpringApplication.run(LendoApplication.class, args);
 	}
 
